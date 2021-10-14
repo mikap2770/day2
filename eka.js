ff34d213bdd5b4b262d6d8b8a257d7831ca04e61
Mika Pennanen <mika.pennanen7@gmail.com>
2021-10-14 13:28:46 +0300

    perustesti valmis

---
+++ eka.js
@@ -0,0 +1,10 @@
+//let on paikallinen muuttuja
+let a = 5;
+let b = 7;
+let sum = a + b;
+console.log("Summa on " + sum);
+
+//var on ns.public
+for(let x=1; x<=3;x++){
+console.log("X arvo on "+x);
+}
